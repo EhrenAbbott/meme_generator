30d646e0cb33450b50f4e4a1f8503e33c65b6f0b
Ehren Abbott <era12358@gmail.com>
2023-09-15 14:01:55 -0500

    Added handleChange function

--- src/Meme.jsx
+++ src/Meme.jsx
@@ -22,21 +22,48 @@ export default function(){
             randomImage: url
         }))
         
+    } 
+
+    function handleChange(event){ 
+        const { name, value } = event.target
+
+        setMeme(prevState => { 
+            return { 
+                ...prevState, 
+                [name]: value,
+            }
+        })
     }
+
+    
     
     return( 
         <div className='meme-div'>  
                 <div className='form-div'> 
                     <div className='text-div'>
-                        <input type="text" placeholder='top text'/> 
-                        <input type="text" placeholder='bottom text'/>
+                        <input  
+                            type="text" 
+                            placeholder='top text'
+                            name="topText"
+                            value={meme.topText}
+                            onChange={handleChange}
+                        /> 
+                        <input 
+                            type="text" 
+                            placeholder='bottom text'
+                            name="bottomText"
+                            value={meme.bottomText}
+                            onChange={handleChange}
+                        />
                     </div>  
                     <div className='button-div'>
                         <button onClick={getMemeImage} className='submit-btn' type="submit">Get new image</button>
                     </div>
                 </div> 
-                <div className='img-div'> 
+                <div className='meme'> 
                     <img className="meme--image" src={meme.randomImage}/>
+                    <h2 className="meme--text top">{meme.topText}</h2>
+                    <h2 className="meme--text bottom">{meme.bottomText}</h2>
                 </div>
         </div>
     )

e31fd5e5baf6e70a95925e4de2430820acb6db85
Ehren Abbott <era12358@gmail.com>
2023-09-19 13:14:25 -0500

    Added textAlignment function

--- src/Meme.jsx
+++ src/Meme.jsx
@@ -6,7 +6,10 @@ export default function(){
     const [meme, setMeme] = React.useState({
         topText: "",
         bottomText: "",
-        randomImage: "http://i.imgflip.com/1bij.jpg" 
+        randomImage: "http://i.imgflip.com/1bij.jpg",
+        pushLeft: false,
+        pushRight: false,
+        diptych: false, 
     })
     
     const [allMemeImages, setAllMemeImages] = React.useState([])
@@ -17,16 +20,79 @@ export default function(){
             .then(data => setAllMemeImages(data.data.memes))
     }, [])
 
+    function textAlignment() { 
+        const pushLeftArr = [ 
+            "https://i.imgflip.com/1nck6k.jpg",
+            "https://i.imgflip.com/38el31.jpg",
+        ] 
+        const diptychArr = [ 
+           "https://i.imgflip.com/345v97.jpg",
+           "https://i.imgflip.com/3lmzyx.jpg",
+           "https://i.imgflip.com/9ehk.jpg",
+           "https://i.imgflip.com/43a45p.png",
+           "https://i.imgflip.com/1tkjq9.jpg",
+           "https://i.imgflip.com/3umnr3.jpg",
+           "https://i.imgflip.com/1ur9b0.jpg", 
+           "https://i.imgflip.com/7ry2sf.png",
+        ] 
+        const pushRightArr = [ 
+            "https://i.imgflip.com/24zoa8.jpg",
+            "https://i.imgflip.com/1iruch.jpg",
+        ]
+
+        if (pushLeftArr.includes(meme.randomImage)) { 
+            console.log("includes")
+            setMeme(prevMeme => {  
+                return { 
+                    ...prevMeme, 
+                    pushLeft: true
+                } 
+            }) 
+            console.log(meme.pushLeft)
+
+        } else if (diptychArr.includes(meme.randomImage)){ 
+            console.log("includes")
+            setMeme(prevMeme => { 
+                return { 
+                    ...prevMeme, 
+                    diptych: true
+                }
+            })
+            console.log(meme.diptych)
+
+        } else if (pushRightArr.includes(meme.randomImage)){ 
+            console.log("includes")
+            setMeme(prevMeme => { 
+                return { 
+                    ...prevMeme, 
+                    pushRight: true
+                }
+            })
+            console.log(meme.diptych)
+    }  
+    }
+
+    function clearText() { 
+        setMeme(prevMeme => { 
+            return { 
+                ...prevMeme, 
+                topText: "", 
+                bottomText: "",
+            }
+        })
+    }
+
     function getMemeImage() {
+        textAlignment()
         const randomNumber = Math.floor(Math.random() * allMemeImages.length)
         const url = allMemeImages[randomNumber].url
+        // const url = "https://i.imgflip.com/1nck6k.jpg"
         console.log(url)
         setMeme(prevMeme => ({
             ...prevMeme,
             randomImage: url,
-            topText: "", 
-            bottomText: "",
         }))
+        clearText()
     } 
 
     function handleChange(event){ 
